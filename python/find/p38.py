6dfe090c87d2f895e413586910d282cf6971b29c
5OH-HO5 <omj0414@daum.net>
2019-12-02 19:59:54 +0900

    mongodb

---
+++ python/find/p38.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import turtle
+
+
+# In[2]:
+
+
+d=100
+
+
+# In[4]:
+
+
+turtle.forward(d)
+
+
+# In[5]:
+
+
+turtle.left(120)
+
+
+# In[7]:
+
+
+turtle.forward(d)
+
+
+# In[8]:
+
+
+import turtle as t
+
+
+# In[9]:
+
+
+t.left(120)
+
+
+# In[10]:
+
+
+t.forward(d)
+
